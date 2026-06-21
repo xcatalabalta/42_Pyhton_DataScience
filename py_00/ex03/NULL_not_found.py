f00260c533bfb91e801378920f7c1f7c84796705
Francisco Javier Català Balta <fcatala-@student.42barcelona.com>
2026-06-21 07:18:24 +0200

    refactor: ex03. Added to work as main or just a function

--- py_00/ex03/NULL_not_found.py
+++ py_00/ex03/NULL_not_found.py
@@ -1,17 +1,56 @@
 def NULL_not_found(object: any) -> int:
-	obj = type(object)
-	name = type(object).__name__
-	print(f"KOSA  {name}  : {object} {obj}")
-	return 0
+    if object is None:
+        name = "Nothing"
+    elif object is False:
+        name = "Fake"
+    elif object == 0:
+        name = "Zero"
+    elif object == '':
+        name = "Empty"
+    elif object != object:
+        name = "Cheese"
+    else:
+        print("Type not Found")
+        return 1
+    print(f"{name}: {object} {type(object)}")
+    return 0
+        
+""" 
+#original function    
+	if object is None:
+        print(f"Nothing: {object} <class 'NoneType'>")
+        return 0
+    elif object is False:
+        print(f"Fake: {object} {type(object)}")
+        return 0
+    elif object == 0:
+        print(f"Zero: {object} <class 'int'>")
+        return 0
+    elif object == '':
+        print(f"Empty: {object} <class 'str'>")
+        return 0
+    elif object != object:
+        print(f"Cheese: {object} <class 'float'>")
+        return 0
+    else:
+        print("Type not Found")
+    return 1 """
 
-Nothing = None
-Garlic = float("NaN")
-Zero = 0
-Empty = ""
-Fake = False
-NULL_not_found(Nothing)
-NULL_not_found(Garlic)
-NULL_not_found(Zero)
-NULL_not_found(Empty)
-NULL_not_found(Fake)
-print(NULL_not_found("Brian"))
+#Uncomment below to run as main
+if __name__ == '__main__':
+    Nothing = None
+    Garlic = float("NaN")
+    Zerito = 0
+    zeroFloat = 0.0
+    Empty = ""
+    Fake = False
+    num = 69
+    NULL_not_found(Nothing)
+    NULL_not_found(Garlic)
+    NULL_not_found(Zerito)
+    NULL_not_found(Empty)
+    NULL_not_found(Fake)
+    NULL_not_found(zeroFloat)
+    print(NULL_not_found("Brian"))
+    print(NULL_not_found(Fake))
+    print(NULL_not_found(num))
--- py_00/ex03/tester.py
+++ py_00/ex03/tester.py
@@ -1,12 +1,13 @@
-from test_not_found import fun
+from NULL_not_found import NULL_not_found
 Nothing = None
 Garlic = float("NaN")
 Zero = 0
 Empty = ""
-Fake = False
+Mentira = False
 
-fun(Nothing)
-#NULL_not_found(Garlic)
-#NULL_not_found(Zero)
-#NULL_not_found(Empty)
-#NULL_not_found(Fake
+NULL_not_found(Nothing)
+NULL_not_found(Garlic)
+NULL_not_found(Zero)
+NULL_not_found(Empty)
+NULL_not_found(Mentira)
+print(NULL_not_found("Brian"))
